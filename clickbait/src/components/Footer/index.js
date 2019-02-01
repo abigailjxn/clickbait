3a3d768a1db5fc70873e02679d0078b158553e3a
Abigail Jackson <41799687+abigailjxn@users.noreply.github.com>
2019-01-31 22:58:34 -0600

    added footer and bootstrap

---
+++ clickbait/src/components/Footer/index.js
@@ -0,0 +1,12 @@
+import React from "react";
+import "./style.css";
+
+function Footer() {
+    return <>
+    <nav className="navbar navbar-light">
+
+    </nav>
+    </>
+}
+
+export default Footer;
