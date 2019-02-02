54f62e3c05c805335775c7b3213ee527f847b668
Abigail Jackson <41799687+abigailjxn@users.noreply.github.com>
2019-02-02 11:29:26 -0600

    Updates

---
+++ precache-manifest.376deb36df8880b621d0e1cdfb4c32d4.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "daa0fc54a81547241788",
+    "url": "/clickbait/static/js/runtime~main.daa0fc54.js"
+  },
+  {
+    "revision": "a086a40676db2db692f5",
+    "url": "/clickbait/static/js/main.a086a406.chunk.js"
+  },
+  {
+    "revision": "c862046c56ad480cf8af",
+    "url": "/clickbait/static/js/1.c862046c.chunk.js"
+  },
+  {
+    "revision": "a086a40676db2db692f5",
+    "url": "/clickbait/static/css/main.b8e8493c.chunk.css"
+  },
+  {
+    "revision": "f79ae15bde8d0eb710eaf97d0eee6d4b",
+    "url": "/clickbait/index.html"
+  }
+];
