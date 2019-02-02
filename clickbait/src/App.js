90573e542954da76ccc7e2fa7be2d38a734ec790
Abigail Jackson <41799687+abigailjxn@users.noreply.github.com>
2019-02-02 01:01:36 -0600

    working userscore update, bug with topscore update

--- clickbait/src/App.js
+++ clickbait/src/App.js
@@ -14,9 +14,36 @@ class App extends Component {
   };
 
 
-handleCardClick = (id) => {
+
+handleCardClick = (id, handleUserScore) => {
   console.log(id)
-}
+
+  this.state.cards.forEach(card => {
+    if (card.id === id ) {
+      if (card.checked === true) {
+        console.log("clicked twice")
+        // function to reset
+      } else {
+        card.checked = true;
+        this.setState({ userScore: this.state.userScore + 1})
+        // bug -- user score logs as 0 on first click
+        // console.log(`this is the user score: ${this.state.userScore}`);
+        if (this.state.userScore > this.state.topScore) {
+          this.setState({topScore: this.state.userScore});
+        };
+        
+       // shuffle cards
+       
+
+        
+      }
+    }
+  });
+
+
+};
+
+
 
 
 render (){
@@ -24,7 +51,7 @@ render (){
    <div>
      <Container>
        <div className="gameboard">
-         {this.state.cards.map((card, i) => <Card key={i} id={card.id} name={card.name} image={card.image} handleCardClick={this.handleCardClick} char={card.id}/>)}
+         {this.state.cards.map((card, i) => <Card key={i} id={card.id} name={card.name} image={card.image} handleCardClick={this.handleCardClick} />)}
        </div>
 
      </Container>
